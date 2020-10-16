abbea4318415c9f37163908619071196322d3c49
Théo BIET <dev.theobiet@gmail.com>
2020-10-16 20:34:47 +0200

    Adding cough animation when you don't have mask, fix problem of taking 40ms (0.1 now), and resolve all of bug

--- cl_main.lua
+++ cl_main.lua
@@ -17,9 +17,9 @@ end)
 local zones = {
     {
         title = "~p~Zone infectée",
-        colour = 40,                -- Pour configurer la couleur blips jetez un oeil à https://wiki.rage.mp/index.php?title=Blips
-        id = 84,                    -- Pour configurer l'ID du blips jetez un oeil à https://wiki.rage.mp/index.php?title=Blips
-        x = -231.04,    
+        colour = 40, -- Pour configurer la couleur blips jetez un oeil à https://wiki.rage.mp/index.php?title=Blips
+        id = 84, -- Pour configurer l'ID du blips jetez un oeil à https://wiki.rage.mp/index.php?title=Blips
+        x = -231.04,
         y = -387.12,
         z = 30.4
     }
@@ -62,16 +62,23 @@ end)
 
 --------------------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------
-----------------   Récupération de la distance vous séparant de l'une des zones  -----------------------------
-----------------           (Vous n'avez probablement pas besoin d'y toucher      -----------------------------
+------------------  	Envoi d'une notification à l'entrée/sortie de la zone infectée    --------------------
+------------------        	Activation / Désactivation des effets à l'écran    	    	  --------------------
+------------------        	    Gestion de la résistance si masque ou non   	    	  --------------------
+------------------        	        Animation "toux" si pas de masque            	      --------------------
+------------------        	    Récupération de la résistance en sortie de zone     	  --------------------
 --------------------------------------------------------------------------------------------------------------
 --------------------------------------------------------------------------------------------------------------
 Citizen.CreateThread(function()
     while not NetworkIsPlayerActive(PlayerId()) do Citizen.Wait(0) end
 
     while true do
-        local playerPed = GetPlayerPed(-1)
-        local x, y, z = table.unpack(GetEntityCoords(playerPed, true))
+        Citizen.Wait(0)
+        local player = GetPlayerPed(-1)
+        local x, y, z = table.unpack(GetEntityCoords(player, true))
+        -----------------------------------------------------------------------------------------------------------
+        -- Récupération de la distance vous séparant de la zone (Vous n'avez probablement pas besoin d'y toucher)--
+        -----------------------------------------------------------------------------------------------------------
         local minDistance = 100000
         for i = 1, #zones, 1 do
             dist = Vdist(zones[i].x, zones[i].y, zones[i].z, x, y, z)
@@ -80,92 +87,54 @@ Citizen.CreateThread(function()
                 closestZone = i
             end
         end
-        Citizen.Wait(15000)
-    end
-end)
-
---------------------------------------------------------------------------------------------------------------
---------------------------------------------------------------------------------------------------------------
-------------------	Envoi d'une notification à l'entrée/sortie de la zone infectée		  --------------------
-------------------        	Activation / Désactivation des effets à l'écran    	    	  --------------------
---------------------------------------------------------------------------------------------------------------
---------------------------------------------------------------------------------------------------------------
-Citizen.CreateThread(function()
-    while not NetworkIsPlayerActive(PlayerId()) do Citizen.Wait(0) end
-
-    while true do
-        Citizen.Wait(0)
-        local player = GetPlayerPed(-1)
-        local x, y, z = table.unpack(GetEntityCoords(player, true))
+        -----------------------------------------------------------------------------------------------------------
+        -----------------------------------------------------------------------------------------------------------
+        -----------------------------------------------------------------------------------------------------------
         local dist = Vdist(zones[closestZone].x, zones[closestZone].y,
                            zones[closestZone].z, x, y, z)
-
+        local seconds = 1000
         if dist <= 2500.0 then
-            StartScreenEffect('DeathFailNeutralIn', 0, true)
             if not notifIn then
-                TriggerEvent("pNotify:SendNotification", {
-                    text = "<b style='color:#fff'>Attention, vous êtes dans un zone infectée.</b>",
-                    type = "error",
-                    timeout = (3000),
-                    layout = "bottomcenter",
-                    queue = "global"
-                })
+                StartScreenEffect('DeathFailNeutralIn', 0, true)
+                ESX.ShowNotification(
+                    "~r~Attention, vous entrez dans une zone infectée, n'y laisser pas votre peau")
+                ESX.ShowNotification("HRP: Vous risquez la mort RP")
                 notifIn = true
                 notifOut = false
             end
+            local maskIndex = GetPedDrawableVariation(player, 1)
+            -- Vérifie si vous portez un masque à gaz (Les numéros correspondent aux ID des masques, en l'occurence ce sont des masques à gaz, mais cela peut varier selon les serveurs et les tenues moddées présentes)              
+            if maskIndex == 129 or maskIndex == 130 or maskIndex == 166 then
+                -- Quantité de résistance à la radiation à retirer toutes les 10 secondes avec un masque                       
+                TriggerEvent('esx_status:remove', 'bionaz', 10000) -- Je retire 10000 de résistance sur 1 million toutes les 10 secondes (~16 minutes)
+                Citizen.Wait((seconds * 10))
+            else
+                -- Si la personne n'a pas de masque, alors elle toussera et perdra plus de résistance
+                RequestAnimDict("timetable@gardener@smoking_joint")
+                while not HasAnimDictLoaded("timetable@gardener@smoking_joint") do
+                    Citizen.Wait(100)
+                end
+                TaskPlayAnim(player, "timetable@gardener@smoking_joint",
+                             "idle_cough", 8.0, 8.0, -1, 50, 0, false, false,
+                             false)
+                Citizen.Wait((seconds * 5))
+                ClearPedTasks(player)
+                -- Quantité de résistance à la radiation à retirer toutes les 10 secondes sans masque 
+                TriggerEvent('esx_status:remove', 'bionaz', 20000) -- Je retire 20000 de résistance sur 1 million toutes les 10 secondes (~8 minutes)
+                Citizen.Wait((seconds * 5))
+            end
         else
             StopScreenEffect('DeathFailNeutralIn', 0, true)
+            ClearPedTasks(player)
             if not notifOut then
-                TriggerEvent("pNotify:SendNotification", {
-                    text = "<b style='color:#fff'>Vous sortez de la zone d'infection.</b>",
-                    type = "success",
-                    timeout = (3000),
-                    layout = "bottomcenter",
-                    queue = "global"
-                })
+                ESX.ShowNotification("~g~Vous sortez de la zone infectée")
                 notifOut = true
                 notifIn = false
             end
-        end
-    end
-end)
-
---------------------------------------------------------------------------------------------------------------
---------------------------------------------------------------------------------------------------------------
---------	Verifie si vous portez un masque et augmente le taux de radiation en conséquence	  ------------
---------	                        Si vous êtes en dehors de la zone	                          ------------
---------------------------------------------------------------------------------------------------------------
---------------------------------------------------------------------------------------------------------------
-Citizen.CreateThread(function()
-    while not NetworkIsPlayerActive(PlayerId()) do Citizen.Wait(0) end
-
-    while true do
-        Citizen.Wait(0)
-        local player = GetPlayerPed(-1)
-        local x, y, z = table.unpack(GetEntityCoords(player, true))
-        local dist = Vdist(zones[closestZone].x, zones[closestZone].y,
-                           zones[closestZone].z, x, y, z)
-
-        if dist <= 2500.0 then
-            local maskIndex = GetPedDrawableVariation(player, 1)
-            local source = _source
-            -- Vérifie si vous portez un masque à gaz (Les numéros correspondent aux ID des masques, en l'occurence ce sont des masques à gaz, mais cela peut varier selon les serveurs et les tenues moddées présentes)              
-            if maskIndex == 129 or maskIndex == 130 or maskIndex == 166 then
-                Citizen.Wait(10000)
-                -- Quantité de résistance à la radiation à retirer toutes les 10 secondes avec un masque                           
-                TriggerEvent('esx_status:remove', 'bionaz', 10000)
-            else
-                Citizen.Wait(10000)
-                -- Quantité de résistance à la radiation à retirer toutes les 10 secondes sans masque 
-                TriggerEvent('esx_status:remove', 'bionaz', 5000)
-            end
-        else 
-            TriggerEvent('esx_status:getStatus', 'bionaz', function(status)
-                while status.val < 1000000 do
-                    Citizen.Wait(10000)
-                    TriggerEvent('esx_status:add', 'bionaz', 5000)
-                end
-			end)
+            -- Une fois la personne sortie de la zone infectée, celle-ci récupère peu à peu sa résistance aux radiations
+            Citizen.Wait((seconds * 3))
+            ClearPedTasks(player)
+            TriggerEvent('esx_status:add', 'bionaz', 3000) -- Je rajoute 10000 de résistance sur 1 million toutes les 3 secondes (~5 minutes)
         end
     end
 end)
